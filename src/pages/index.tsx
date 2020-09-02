14d7c03db26cefbe7ed6a9d414a8aaf178ea6f56
Matheuscsceil <math.cs.ceil@gmail.com>
2020-09-02 13:37:44 -0300

    Structure ReactJS + NextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+
+// import Logo from '../assets/logo.svg'
+import error from '../assets/error.png'
+
+import {Container} from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      {/* <Logo /> */}
+      <img src={error} alt="" />
+      <h1>
+        Hello World!
+      </h1>
+    </Container>
+  )
+}
+
+export default Home
